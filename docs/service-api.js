81d94aeb345405edb674a0d111863602fcf8b618
unknown <andrew.baumann@ENGINEERING.SNOW>
2026-04-21 12:17:34 -0600

    got baseURL and drawio working

--- docs/service-api.js
+++ docs/service-api.js
@@ -1,20 +1,21 @@
 const baseUrl = "https://one-oxl7.onrender.com"
+// const baseUrl = "https://localhost"
 
-export const GetData = async () => {
-    const data = await fetch();
-    const filteredData = await data.json();
-    return filteredData;
-}
+// export const GetData = async () => {
+//     const data = await fetch();
+//     const filteredData = await data.json();
+//     return filteredData;
+// }
 
-export const SendData = async (data) => {
-    await fetch("", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(contact),
-    });
-}
+// export const SendData = async (data) => {
+//     await fetch("", {
+//         method: "POST",
+//         headers: {
+//             "Content-Type": "application/json",
+//         },
+//         body: JSON.stringify(contact),
+//     });
+// }
 
 export const GetNewDeck = async () => {
     const Deck = await fetch(`${baseUrl}/NewDeck`);
